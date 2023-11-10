c15ef244dbf95bdbc0c63a0a8ba6fc39f71c1d59
zBerryBoost <150356655+zBerryBoost@users.noreply.github.com>
2023-11-10 17:51:57 +0500

    Update test.js

--- test.js
+++ test.js
@@ -3,16 +3,22 @@ const express = require('express');
 const router = express.Router();
 
 
+const app =express();
 
 
-router.post("/", async(req, res)=>{
-    try{
-        res.json("hello this is test file")
+app.get("/", (req, res) => {
+     console.log("hello data");
+});
 
-    }catch(error){
-        console.error("Error user:",error);
-        res.status(500).json({error: "internal server error"});
-    }
+    
+// router.post("/", async(req, res)=>{
+//     try{
+//         res.json("hello this is test file")
+
+//     }catch(error){
+//         console.error("Error user:",error);
+//         res.status(500).json({error: "internal server error"});
+//     }
 });
 
 
